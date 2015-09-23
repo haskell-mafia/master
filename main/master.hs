4c9293b3abbdb390a64bb4a5d026c14814f7455c
Nick Hibberd <nhibberd@gmail.com>
2015-09-23 14:33:40 +1000

    Adding runner to cli

--- main/master.hs
+++ main/master.hs
@@ -9,9 +9,13 @@ import           Options.Applicative
 import           Master
 
 import           P
+import qualified Prelude as PE
 
-import           System.IO
+import           System.Directory
+import           System.FilePath
+import           System.Environment
 import           System.Exit (exitFailure, exitSuccess)
+import           System.IO
 
 import           X.Options.Applicative
 
@@ -22,14 +26,16 @@ main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
+  m <- lookupEnv "MASTER_CACHE"
+  h <- getHomeDirectory
+  let cache = maybe (h </> ".master" </> "cache") id m
   dispatch (safeCommand commandP) >>= \sc -> case sc of
     VersionCommand ->
       (putStrLn $ "master: " <> buildInfoVersion) >> exitSuccess
     RunCommand DryRun c ->
       print c >> exitFailure
     RunCommand RealRun (BuildCommand _) ->
-      putStrLn "not-implemented" >> exitFailure
-
+      orDie renderRunnerError $ runner cache PE.undefined PE.undefined
 
 commandP :: Parser Command
 commandP =  subparser $
